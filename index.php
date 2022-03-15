9384f3c6d3da29f2811f3101cb263bf4e624564e
Nuzbit <87460840+Nuzbit01@users.noreply.github.com>
2022-03-15 15:47:28 -0600

    Cambiando textos de sesion

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Cambio de titulo </title>
+    <title>CoAsT</title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
