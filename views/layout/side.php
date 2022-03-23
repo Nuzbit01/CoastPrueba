6c437245ee4745d4d8ac966b836ebb2a24c75269
Nuzbit <87460840+Nuzbit01@users.noreply.github.com>
2022-03-22 22:14:26 -0600

    Cambiando direcciones

--- views/layout/side.php
+++ views/layout/side.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?=base_url?>alumno/index">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?=base_url?>alumno/dashboard">
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div>
@@ -14,7 +14,7 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item active">
-        <a class="nav-link" href="<?=base_url?>alumno/index">
+        <a class="nav-link" href="<?=base_url?>alumno/dashboard">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span>
         </a>
